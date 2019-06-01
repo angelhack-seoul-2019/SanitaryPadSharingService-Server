c69f453be86a80f885e06d88e938082c7604656b
= <=>
2019-06-01 11:20:07 +0000

    organizationController - create getlist

--- app/Http/Controllers/OrganiController.php
+++ app/Http/Controllers/OrganiController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Organization;
 
 class OrganiController extends Controller
 {
-    //
+	const RESULT_SUC = 200;
+	const RESULT_ERR = 404;
+
+	public function getList(){
+		$list = Organization::get();
+		if($list->isEmpty()){
+			return array(
+				"result"=>self::RESULT_ERR
+			);
+		}
+
+		return array(
+			"result"=>self::RESULT_SUC,
+			"data"=>$list
+		);
+	}
+		
 }

163436099fa205e90412396f0a662508336cce4f
= <=>
2019-06-01 10:53:26 +0000

    create Organization Model

---
+++ app/Model/Organization.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+	protected $table = 'organization';
+	public $timestamps = false;
+	protected $primaryKey = 'id';
+	protected $fillable = ['name', 'lat', 'lon'];
+}
